f14e8b5f84d426b3fe5f4d69177a088f1c622b0d
4x3l15 <adramirez@escuelasproa.edu.ar>
2023-12-03 19:12:38 -0300

    ..

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-// script.js
 document.addEventListener('DOMContentLoaded', function() {
-    // Código que se ejecuta cuando el DOM ha sido completamente cargado
     console.log('DOM cargado');
 
-    // Agregar un evento al botón
-    var miBoton = document.getElementById('miBoton');
+
+const miBoton = document.getElementById('miBoton');
     miBoton.addEventListener('click', function() {
         alert('¡Botón clicado!');
     });
