f0e875180766441c6717af31c7055fb76cc0713b
4x3l15 <adramirez@escuelasproa.edu.ar>
2023-12-05 10:23:24 -0300

    ..

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-// script.js
+const express = requiere ('express');
+const app = ('');
+const mongoose = require('mongoose');
+const port= 3000
+
+mongoose.connect('mongodb+srv://bdiaz:unodostres@clusterr.xffnu0h.mongodb.net/',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+})
+.then(()=>{
+    console.log('Conexion exitosa')
+})
+.catch((err) =>{
+    console.error('error al conectar el servidor :'+err) 
+});
+  
+app.listen(port, () => {
+    console.log('hola, el servidor anda');
+  });
+
 document.addEventListener('DOMContentLoaded', function() {
     console.log('DOM cargado');
 
