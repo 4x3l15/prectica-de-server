d02004d08c8c43bbf4c2645f2e27da9c4f68e6bd
4x3l15 <adramirez@escuelasproa.edu.ar>
2023-12-13 12:19:25 -0300

    .

--- script.js
+++ script.js
@@ -1,38 +1,25 @@
-const express = requiere ('express');
-const app = ('');
+const express = require('express');
+const app = express(); // Inicializar la aplicación Express
 const mongoose = require('mongoose');
-const port= 3000
+const port = 3000;
 
-mongoose.connect('mongodb+srv://adramirez:4x3l__15@clusterr.xffnu0h.mongodb.net/',{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
+mongoose.connect('mongodb+srv://adramirez:4x3l__15@clusterr.xffnu0h.mongodb.net/', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
-.then(()=>{
-    console.log('Conexion exitosa')
-})
-.catch((err) =>{
-    console.error('error al conectar el servidor :'+err) 
-});
+    .then(() => {
+        console.log('Conexion exitosa');
 
-app.listen(port, () => {
-    console.log('hola, el servidor anda');
-  });
-
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('DOM cargado');
-
-    var miBoton = document.getElementById('miBoton');
-    miBoton.addEventListener('click', function() {
-        // Obtener los valores de los inputs
-        var nombre = document.getElementById('nombre').value;
-        var apellido = document.getElementById('apellido').value;
-        var edad = document.getElementById('edad').value;
-
-        // Mostrar los valores en la consola
-        console.log('Nombre:', nombre);
-        console.log('Apellido:', apellido);
-        console.log('Edad:', edad);
-
-        // Aquí puedes agregar lógica adicional, como enviar los datos a un servidor, etc.
+        // Iniciar la aplicación Express después de la conexión exitosa
+        app.listen(port, () => {
+            console.log('El servidor está escuchando en el puerto ' + port);
+        });
+    })
+    .catch((err) => {
+        console.error('Error al conectar el servidor: ' + err);
     });
-});
+
+// Código de manipulación del DOM debería ir en archivos HTML y JS separados, no aquí.
+
+// Puedes dejar el bloque de código de manipulación del DOM si estás usando Express para servir archivos estáticos.
+app.use(express.static('public'));
